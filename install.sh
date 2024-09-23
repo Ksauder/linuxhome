41c718ea22e4ebd05db2fd043bd7b5ec53c9a5c7
ksauder <ksauder.dev@gmail.com>
2024-09-23 00:14:05 -0400

    oops

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# this script won't be useful unless the repo is made public so unauthed requests can download it
 git clone --recurse-submodules https://github.com/Ksauder/linuxhome ~/.homerepo
 
