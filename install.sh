c60f901144e3dd9d50711a0afd7fd97ab39cde65
ksauder <ksauder.dev@gmail.com>
2024-09-24 09:23:16 -0400

    run bootstrap.sh in install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 # this script won't be useful unless the repo is made public so unauthed requests can download it
 git clone --recurse-submodules https://github.com/Ksauder/linuxhome ~/.homerepo
+cd ~/.homerepo && ./bootstrap.sh
 
