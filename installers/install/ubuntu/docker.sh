7133d3a8679d934eab232545972e7e0fc0558a9e
Ksauder <ksauder.dev@gmail.com>
2024-12-15 23:37:49 -0500

    add support for ubuntu

---
+++ installers/install/ubuntu/docker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+remove_packages docker.io docker-doc docker-compose podman-docker containerd runc
+install -m 0755 -d /etc/apt/keyrings
+root_shell curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+root_shell chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    root_shell tee /etc/apt/sources.list.d/docker.list >/dev/null
+install_packages docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# allow user to use docker
+root_shell groupadd docker || true
+root_shell usermod -aG docker $USER
+root_shell systemctl enable docker.service
+root_shell systemctl enable containerd.service
