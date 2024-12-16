fb4bf6c26e026a14e286aab76d74e9e1263d0da3
Ksauder <ksauder.dev@gmail.com>
2024-12-15 23:37:30 -0500

    fix neovim pkg script

--- installers/install/common/neovim.sh
+++ installers/install/common/neovim.sh
@@ -2,8 +2,8 @@
 
 echo "Installing Neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-rm -rf /opt/nvim
-tar -C /opt -xzf nvim-linux64.tar.gz
+root_shell rm -rf /opt/nvim
+root_shell tar -C /opt -xzf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.rc_bash_zsh.local
 echo "Done installing Neovim"
