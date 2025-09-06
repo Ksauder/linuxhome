51f6dd64901941c172213109bb91824739ea45c3
Ksauder <ksauder.dev@gmail.com>
2025-09-05 23:33:12 -0400

    fix HOME usage and sudo usage in a two scripts

--- installers/common/homebrew.sh
+++ installers/common/homebrew.sh
@@ -5,6 +5,6 @@ set -e
 sudo apt-get update
 sudo apt-get install build-essential gcc
 
-echo >>/home/kyle/.zshrc.local
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>/home/kyle/.zshrc.local
+echo >>${HOME}/.zshrc.local
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>${HOME}/.zshrc.local
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
