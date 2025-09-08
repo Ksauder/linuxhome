58b170d12995057298dbe1a878238716e69ea7f7
Ksauder <ksauder.dev@gmail.com>
2025-09-08 11:12:42 -0400

    massive changes, working to sharpen the point and simplify. Moved python into a repo

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 if ! command -v git 2>&1 >/dev/null; then
     echo "Git must be installed"
-    exit 1;
+    exit 1
 fi
 
 git clone --recurse-submodules git@github.com:Ksauder/linuxhome.git ~/.homerepo
-cd ~/.homerepo && ./homerepo bootstrap
-
+# cd ~/.homerepo && ./homerepo bootstrap
--- lib/pyhomerepo/tests/test_basics.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ${REPO_ROOT}/lib/sh/basic_utils_lib.sh
-${REPO_ROOT}/homerepo bootstrap
-sudo ${REPO_ROOT}/scripts/basics.sh ${1}
