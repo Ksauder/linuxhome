1da896ba18ebfd44214c8dd0e23747439eb16e38
Ksauder <ksauder.dev@gmail.com>
2025-11-12 13:13:10 -0500

    update the neovim installer, used it to update neovim to latest

--- installers/debian/neovim.sh
+++ installers/debian/neovim.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 echo "Installing Neovim"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.rc_bash_zsh.local
+curl -LO --fail-with-body https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzvsf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+ln -fs /opt/nvim-linux-x86_64/bin/nvim ~/.local/bin/nvim
+#echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.rc_bash_zsh.local
 echo "Done installing Neovim"
 
